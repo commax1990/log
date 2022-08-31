2c0a531f322d235a8831a5f74408541de1989b56
commax1990 <legioner_list@mail.ru>
2022-08-31 20:46:45 +0500

    Авторизация
    Signed-off-by: commax1990 <legioner_list@mail.ru>

---
+++ mysite/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+
+    # path('add_news', views.add_news, name='add_news')
+
+]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request, 'login.html')
+    # if request.method == 'POST':
+    #     form = UserLoginForm(data=request.POST)
+    #     if form.is_valid():
+    #         user = form.get_user()
+    #         login(request,user)
+    #         return redirect('index')
+    # else:
+    #     form = UserLoginForm()
+    # return render(request,'pages/autorizaciya.html',{'form': form})

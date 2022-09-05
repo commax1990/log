b6834e33f6e9fda27fca90736c9a62610514b048
commax1990 <legioner_list@mail.ru>
2022-09-06 01:15:13 +0500

    Авторизация
    Signed-off-by: commax1990 <legioner_list@mail.ru>

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.UserLogin, name='UserLogin'),
 
     # path('add_news', views.add_news, name='add_news')
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,19 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
+
+from .forms import UserLoginForm
+
+
+def UserLogin(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserLoginForm()
+    return render(request, 'pages/login.html', {'form': form})
 
 
 def login(request):

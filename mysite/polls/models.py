cdcaad216b74a6b2105e20bf6d8d598cbe5e5ce3
commax1990 <legioner_list@mail.ru>
2022-09-05 23:35:52 +0500

    Добавление модели
    Signed-off-by: commax1990 <legioner_list@mail.ru>

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.db import models
+
+
+class Spisok_MC_Number(models.Model):
+    mcnumber = models.TextField(blank=50, verbose_name='name')
+
+    def __str__(self):
+        return self.mcnumber
+
+    class Meta:
+        verbose_name = 'mcnumber'
+        verbose_name_plural = 'mcnumber'
+        ordering = ['mcnumber']

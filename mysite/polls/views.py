80c2530f6dbf29114b511482499207d77aae98a7
commax1990 <legioner_list@mail.ru>
2022-09-17 11:17:59 +0500

    Авторизация
    Signed-off-by: commax1990 <legioner_list@mail.ru>

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,6 @@ def UserLogin(request):
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            # login(request, user)
             return render(request, 'pages/index.html')
     else:
         form = UserLoginForm()
@@ -18,12 +17,3 @@ def UserLogin(request):
 def login(request):
     return render(request, 'pages/login.html')
 
-    # if request.method == 'POST':
-    #     form = UserLoginForm(data=request.POST)
-    #     if form.is_valid():
-    #         user = form.get_user()
-    #         login(request,user)
-    #         return redirect('index')
-    # else:
-    #     form = UserLoginForm()
-    # return render(request,'pages/autorizaciya.html',{'form': form})
